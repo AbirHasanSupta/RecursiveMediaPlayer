51d30dea94ebcfbcf7bb9d36ef8e4f00e6097ae0
Supto <abirhasan.supta254@gmail.com>
2025-07-11 02:57:38 +0600

    Implements video player moving from one monitor to another

--- app.py
+++ app.py
@@ -37,19 +37,37 @@ def set_window_pos(hwnd, x, y, w, h):
 class VLCPlayerController:
     def __init__(self, videos):
         monitors = get_monitors()
-        if len(monitors) > 1:
-            mon = monitors[1]
-            print(f"Using second monitor: {mon.name} at ({mon.x}, {mon.y}), {mon.width}x{mon.height}")
 
-            self.monitor2_x = mon.x
-            self.monitor2_y = mon.y
-            self.monitor2_width = mon.width
-            self.monitor2_height = mon.height
-
-            self.instance = vlc.Instance(f'--video-x={mon.x}', f'--video-y={mon.y}')
+        if len(monitors) >= 1:
+            mon1 = monitors[0]
+            print(f"Monitor 1: {mon1.name} at ({mon1.x}, {mon1.y}), {mon1.width}x{mon1.height}")
+            self.monitor1_x = mon1.x
+            self.monitor1_y = mon1.y
+            self.monitor1_width = mon1.width
+            self.monitor1_height = mon1.height
         else:
-            mon = monitors[0]
-            print(f"Only one monitor detected: {mon.name}")
+            print("No monitors detected!")
+            self.monitor1_x = 0
+            self.monitor1_y = 0
+            self.monitor1_width = 800
+            self.monitor1_height = 600
+
+        if len(monitors) >= 2:
+            mon2 = monitors[1]
+            print(f"Monitor 2: {mon2.name} at ({mon2.x}, {mon2.y}), {mon2.width}x{mon2.height}")
+            self.monitor2_x = mon2.x
+            self.monitor2_y = mon2.y
+            self.monitor2_width = mon2.width
+            self.monitor2_height = mon2.height
+            # Use monitor 2 as default
+            self.instance = vlc.Instance(f'--video-x={mon2.x}', f'--video-y={mon2.y}')
+        else:
+            print("Only one monitor detected, second monitor features disabled.")
+            # Set monitor 2 same as monitor 1 if not available
+            self.monitor2_x = self.monitor1_x
+            self.monitor2_y = self.monitor1_y
+            self.monitor2_width = self.monitor1_width
+            self.monitor2_height = self.monitor1_height
             self.instance = vlc.Instance()
 
         self.player = self.instance.media_player_new()
@@ -59,6 +77,17 @@ class VLCPlayerController:
         self.lock = threading.Lock()
         self.running = True
         self.fullscreen_enabled = False
+        self.current_monitor = 2
+
+    def position_on_monitor1(self):
+        time.sleep(0.5)
+        hwnd = self.player.get_hwnd()
+        if hwnd:
+            print(f"Window handle: {hwnd}")
+            result = set_window_pos(hwnd, self.monitor1_x, self.monitor1_y,
+                                    self.monitor1_width, self.monitor1_height)
+            print(f"SetWindowPos result on monitor 1: {result}")
+            self.current_monitor = 1
 
     def position_on_monitor2(self):
         time.sleep(0.5)
@@ -66,8 +95,9 @@ class VLCPlayerController:
         if hwnd:
             print(f"Window handle: {hwnd}")
             result = set_window_pos(hwnd, self.monitor2_x, self.monitor2_y,
-                          self.monitor2_width, self.monitor2_height)
-            print(f"SetWindowPos result: {result}")
+                                    self.monitor2_width, self.monitor2_height)
+            print(f"SetWindowPos result on monitor 2: {result}")
+            self.current_monitor = 2
 
     def play_video(self, index):
         with self.lock:
@@ -121,9 +151,11 @@ class VLCPlayerController:
             self.fullscreen_enabled = not self.fullscreen_enabled
             self.player.set_fullscreen(self.fullscreen_enabled)
             if not self.fullscreen_enabled:
-                self.position_on_monitor2()
+                if self.current_monitor == 1:
+                    self.position_on_monitor1()
+                else:
+                    self.position_on_monitor2()
             print(f"Fullscreen set to {self.fullscreen_enabled}")
-
 
     def toggle_pause(self):
         with self.lock:
@@ -142,6 +174,35 @@ class VLCPlayerController:
             if state == vlc.State.Ended:
                 self.next_video()
 
+    def switch_to_monitor(self, monitor_number):
+        with self.lock:
+            print(f"Switching to monitor {monitor_number} by recreating player")
+
+            current_position = self.player.get_time()
+            current_media = self.player.get_media()
+            was_playing = self.player.is_playing()
+
+            self.player.stop()
+
+            if monitor_number == 1:
+                self.instance = vlc.Instance(f'--video-x={self.monitor1_x}', f'--video-y={self.monitor1_y}')
+                self.current_monitor = 1
+            else:
+                self.instance = vlc.Instance(f'--video-x={self.monitor2_x}', f'--video-y={self.monitor2_y}')
+                self.current_monitor = 2
+
+            self.player = self.instance.media_player_new()
+
+            if current_media:
+                self.player.set_media(current_media)
+                if was_playing:
+                    self.player.play()
+                    time.sleep(0.5)
+                    self.player.set_time(current_position)
+                    if self.fullscreen_enabled:
+                        self.player.set_fullscreen(True)
+
+            print(f"Switched to monitor {monitor_number}")
 
 
 def listen_keys(controller):
@@ -152,6 +213,8 @@ def listen_keys(controller):
     keyboard.add_hotkey('down', lambda: controller.volume_down())
     keyboard.add_hotkey('f', lambda: controller.toggle_fullscreen())
     keyboard.add_hotkey('space', lambda: controller.toggle_pause())
+    keyboard.add_hotkey('1', lambda: controller.switch_to_monitor(1))
+    keyboard.add_hotkey('2', lambda: controller.switch_to_monitor(2))
     keyboard.wait('esc')
 
 
